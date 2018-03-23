8aa608725e1b4e9e3fe3df329dd66aa7171b1b10
Sam <samyankun@gmail.com>
2018-03-24 07:44:50 +0800

    [Mod] Allow new block submiting
    Nedd to have a detail check.Because IBD is a right thing.V0.12 is a good example for that.
    
    Signed-off-by: Sam <samyankun@gmail.com>

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -441,7 +441,7 @@ UniValue getblocktemplate(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Bitcoin is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Bitcoin is downloading blocks...");
+        //throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Bitcoin is downloading blocks...");
 
     static unsigned int nTransactionsUpdatedLast;
 
